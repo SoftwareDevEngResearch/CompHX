cd6fb15591b51a8a62b99ff41a67133b5d8f408b
Tommy Moore <tommy.moore22@gmail.com>
2019-04-25 01:52:28 -0700

    Modifying files for travis integration

--- HX_analyze.py
+++ HX_analyze.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import pytest
+#import pytest
 
 def log_mean_temp_diff_counter(temp_hot_in,temp_hot_out,temp_cold_in,temp_cold_out):
     """ Computes the LMTD for a counter-current HX """
@@ -30,35 +30,6 @@ def log_mean_temp_diff_parallel(temp_hot_in,temp_hot_out,temp_cold_in,temp_cold_
     
     return (del_t_1 - del_t_2)/np.log(del_t_1/del_t_2)
 
-def test_lmtd_counter():
-    assert log_mean_temp_diff_counter(100,85,30,55) == pytest.approx(49.83,.005)
-
-def test_lmtd_counter2():
-    with pytest.raises(ValueError):
-        log_mean_temp_diff_counter(10,10,10,10)
-
-def test_lmtd_counter3():
-    with pytest.raises(ValueError):
-        log_mean_temp_diff_counter(10,100,10,100)
-        
-def test_lmtd_counter4():
-    with pytest.raises(ValueError):
-        log_mean_temp_diff_counter(10,100,100,10)
-            
-def test_lmtd_parallel():
-    assert log_mean_temp_diff_parallel(100,85,30,55) == pytest.approx(47.21,.005)
-
-def test_lmtd_parallel2():
-    with pytest.raises(ValueError):
-        log_mean_temp_diff_parallel(10,10,10,10)
-        
-def test_lmtd_parallel3():
-    with pytest.raises(ValueError):
-        log_mean_temp_diff_parallel(10,100,100,10)
-        
-def test_lmtd_parallel4():
-    with pytest.raises(ValueError):
-        log_mean_temp_diff_parallel(100,10,100,10)
 
 def q_lmtd_counter(U,area,temp_hot_in,temp_hot_out,temp_cold_in,temp_cold_out):
     
@@ -74,20 +45,6 @@ def q_lmtd_parallel(U,area,temp_hot_in,temp_hot_out,temp_cold_in,temp_cold_out):
     
     return U*area*log_mean_temp_diff_parallel(temp_hot_in,temp_hot_out,temp_cold_in,temp_cold_out)
 
-def test_q_lmtd_counter():
-    assert q_lmtd_counter(1,1,100,85,30,55) == pytest.approx(49.83,.005)
-    
-def test_q_lmtd_counter2():
-    with pytest.raises(ValueError):
-        q_lmtd_counter(-11,1,100,-85,30,-55)
-    
-def test_q_lmtd_parallel():
-    assert q_lmtd_parallel(1,1,100,85,30,55) == pytest.approx(47.21,.005)
-    
-def test_q_lmtd_parralel2():
-    with pytest.raises(ValueError):
-        q_lmtd_parallel(-11,1,100,-85,30,-55)
-
 def c_min(mass_flow_rate_hot, spec_heat_hot, mass_flow_rate_cold, spec_heat_cold):
     
     c_hot = mass_flow_rate_hot*spec_heat_hot
@@ -97,21 +54,3 @@ def c_min(mass_flow_rate_hot, spec_heat_hot, mass_flow_rate_cold, spec_heat_cold
         raise ValueError("A non-zero c_min value should be specified")
     
     return min(c_hot,c_cold)
-
-def test_c_min_cold():
-    assert c_min(10,10,1,1) == 1
-    
-def test_c_min_hot():
-    assert c_min(1,1,10,10) == 1
-    
-def test_c_min_equal():
-    assert c_min(2,2,2,2) == 4
-    
-def test_c_min_zero():
-    with pytest.raises(ValueError):
-        c_min(0,1,0,2)
-
-
-
-
-#if __name__ == '__main__':
