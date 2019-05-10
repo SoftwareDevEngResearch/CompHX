5463e6a5734d7674204648429729556f57d804ec
Tommy Moore <tommy.moore22@gmail.com>
2019-05-09 21:20:36 -0700

    Updating testing BC

--- test_BC.py
+++ test_BC.py
@@ -2,7 +2,7 @@
 
 #import numpy as np
 import pytest
-from . import HX_boundary_condition as bc
+from . import HX_boundary_cond as bc
 
 def test_temp_bc():
     assert bc.set_boundary_conditions() == (300, 250, 200, 220)
